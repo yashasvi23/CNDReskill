49c71c9624af1ce400d0585cef329ca967f5b7ba
Yashasvi <yashasvi.s@infosys.com>
2021-05-19 16:39:38 +0530

    entity dto

---
+++ Order-MS/src/main/java/com/infy/entity/ProductsOrdered.java
@@ -0,0 +1,55 @@
+package com.infy.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name="productsOrdered")
+@IdClass(CompositeKey.class)
+public class ProductsOrdered {
+	
+	@Id
+	private Integer buyerId;
+	@Id
+	private Integer prodId;
+	
+	private Integer sellerId;
+	
+	private Integer quantity;
+
+	public Integer getBuyerId() {
+		return buyerId;
+	}
+
+	public void setBuyerId(Integer buyerId) {
+		this.buyerId = buyerId;
+	}
+
+	public Integer getProdId() {
+		return prodId;
+	}
+
+	public void setProdId(Integer prodId) {
+		this.prodId = prodId;
+	}
+
+	public Integer getSellerId() {
+		return sellerId;
+	}
+
+	public void setSellerId(Integer sellerId) {
+		this.sellerId = sellerId;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+}
